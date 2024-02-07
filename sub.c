780f0151c7ba48a4befeb9d7301579b027e064fb
Bhanu26032 <bhanu26032@gmail.com>
2024-02-07 17:07:48 +0530

    This is subtraction

---
+++ sub.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	int a,b,result;
+	printf("Enter a value");
+	scanf("%d",&a);
+	printf("Enter b value");
+	scanf("%d",&b);
+	resutl=(a-b);
+	printf("The result of subtraction is %d",result);
+}
